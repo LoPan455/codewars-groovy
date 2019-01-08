ba7fbbd280922cfb10b3ba5ccfaf6f2b0d4d9806
Thomas Johander <thomas.johnader@target.com>
2019-01-08 16:48:30 -0600

    WIP on Hungarian Vowel Harmony

--- src/main/groovy/com/example/demo/Kata.groovy
+++ src/main/groovy/com/example/demo/Kata.groovy
@@ -184,89 +184,96 @@ class Kata {
   static def timedReading(maxLength, String text) {
 
     def pattern = /\b([A-Za-z]{1,$maxLength})\b/
-    def matches = (text =~/$pattern/)
+    def matches = (text =~ /$pattern/)
 
     return matches.size()
 
   }
 
-    /**
-     *
-     * @param l
-     * @param w
-     * @param h
-     * @return
-     */
-    static String wallpaper(double l, double w, double h) {
+  /**
+   *
+   * @param l
+   * @param w
+   * @param h
+   * @return
+   */
+  static String wallpaper(double l, double w, double h) {
 
-        // Constants
-        List<String> numbers = [
-                "zero", "one", "two", "three", "four",
-                "five", "six", "seven", "eight", "nine", "ten",
-                "eleven", "twelve", "thirteen", "fourteen", "fifteen",
-                "sixteen", "seventeen", "eighteen", "nineteen", "twenty"
-        ]
+    // Constants
+    List<String> numbers = [
+        "zero", "one", "two", "three", "four",
+        "five", "six", "seven", "eight", "nine", "ten",
+        "eleven", "twelve", "thirteen", "fourteen", "fifteen",
+        "sixteen", "seventeen", "eighteen", "nineteen", "twenty"
+    ]
 
-        double wallpaperRollWidth = 0.52
-        double wallpaperRollLength = 10
-        double totalAreaOfaRoll = wallpaperRollWidth * wallpaperRollLength
-        double fudgeFactor = 0.15
+    double wallpaperRollWidth = 0.52
+    double wallpaperRollLength = 10
+    double totalAreaOfaRoll = wallpaperRollWidth * wallpaperRollLength
+    double fudgeFactor = 0.15
 
-        // Wall calculations
-        double areaOfTwoWalls = 2 * (l * h)
-        double areaOfTwoOtherWalls = 2 * (w * h)
-        double areaOfAllWalls = areaOfTwoWalls + areaOfTwoOtherWalls
+    // Wall calculations
+    double areaOfTwoWalls = 2 * (l * h)
+    double areaOfTwoOtherWalls = 2 * (w * h)
+    double areaOfAllWalls = areaOfTwoWalls + areaOfTwoOtherWalls
 
-        // Needed wallpaper
-        double numberOfRollsToCover = areaOfAllWalls / totalAreaOfaRoll
-        double totalLengthNeeded = (numberOfRollsToCover + (numberOfRollsToCover * fudgeFactor))
+    // Needed wallpaper
+    double numberOfRollsToCover = areaOfAllWalls / totalAreaOfaRoll
+    double totalLengthNeeded = (numberOfRollsToCover + (numberOfRollsToCover * fudgeFactor))
 
-        int roundedValue = Math.ceil(totalLengthNeeded)
+    int roundedValue = Math.ceil(totalLengthNeeded)
 
-        return numbers[roundedValue]
+    return numbers[roundedValue]
+  }
+
+  /**
+   * In this kata you should simply determine, whether a given year is a leap year or not. In case you don't know the rules, here they are:
+   *
+   * years divisible by 4 are leap years
+   * but years divisible by 100 are no leap years
+   * but years divisible by 400 are leap years
+   * Additional Notes:
+   *
+   * Only valid years (positive integers) will be tested, so you don't have to validate them
+   * @param year
+   */
+  static def isLeapYear(year) {
+
+    boolean result = false
+
+    if ((((year % 4) == 0) && ((year % 100) != 0)) || (((year % 400) == 0))) {
+      result = true
     }
 
-    /**
-     * In this kata you should simply determine, whether a given year is a leap year or not. In case you don't know the rules, here they are:
-     *
-     * years divisible by 4 are leap years
-     * but years divisible by 100 are no leap years
-     * but years divisible by 400 are leap years
-     * Additional Notes:
-     *
-     * Only valid years (positive integers) will be tested, so you don't have to validate them
-     * @param year
-     */
-    static def isLeapYear(year) {
+    return result
+  }
 
-        boolean result = false
+  /**
+   *
+   * @param w
+   * @return
+   */
+  static def dative(String w) {
 
-        if ((((year % 4) == 0) && ((year % 100) != 0)) || (((year % 400) == 0 ))) {
-            result = true
-        }
+    List<String> frontVowels = ["e", "é", "i", "í", "ö", "ő", "ü", "ű"]
+    List<String> backVowels = ["a", "á", "o", "ó", "u", "ú"]
+    String frontVowelSuffix = "nek"
+    String backVowelSuffix = "nak"
 
-        return result
+    frontVowels.each {
+      if ((w.lastIndexOf(it)) != -1) {
+        w += frontVowelSuffix
+      }
     }
 
-    /**
-     *
-     * @param w
-     * @return
-     */
-    static def dative(String w) {
-
-        List<String> frontVowels = ["e", "é", "i", "í", "ö", "ő", "ü", "ű"]
-        List<String> backVowels = ["a", "á", "o", "ó", "u", "ú"]
-        String frontVowelSuffix = "nek"
-        String backVowelSuffix = "nak"
-
-        // TODO: need a better way to find the last instance of one of these vowels
-        def lastVowel = w.findLastIndexOf { frontVowels }
-
-        return w
-
-
+    backVowels.each {
+      if ((w.lastIndexOf(it)) != -1) {
+        w += backVowelSuffix
+      }
     }
+
+    return w
+  }
 
 }
 
--- src/test/groovy/com/example/demo/DemoApplicationTests.groovy
+++ src/test/groovy/com/example/demo/DemoApplicationTests.groovy
@@ -99,9 +99,15 @@ class DemoApplicationTests {
 
    @Test
    void "Hungarian Vowel Harmony Tests"() {
-       assert Kata.dative("ablak") == "ablaknak"
-       assert Kata.dative("szék") == "széknek"
-       assert Kata.dative("otthon") == "otthonnak"
+     assert Kata.dative("ablak") == "ablaknak"
+     assert Kata.dative("tükör") == "tükörnek"
+     assert Kata.dative("keret") == "keretnek"
+     assert Kata.dative("otthon") == "otthonnak"
+     assert Kata.dative("virág") == "virágnak"
+     assert Kata.dative("tett") == "tettnek"
+     assert Kata.dative("rokkant") == "rokkantnak"
+     assert Kata.dative("rossz") == "rossznak"
+     assert Kata.dative("gonosz") == "gonosznak"
    }
 
 }
